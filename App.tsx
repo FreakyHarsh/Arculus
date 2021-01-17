d763cf7df5c93db788feff17457b8b29ee10af12
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-01-17 18:39:06 +0530

    feat: pusehd

--- App.tsx
+++ App.tsx
@@ -5,25 +5,25 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import RequisitionsScreen from './src/screens/Requisition/RequisitionScreen';
 import { DrawerParamList } from './src/types/DrawerParamList';
 
-// const MyTheme = {
-//   ...DefaultTheme,
-//   colors: {
-//     ...DefaultTheme.colors,
-//     primary: 'red',
-//     background: '#FAFAFA',
-//   },
-// };
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1B435C',
+    background: '#FAFAFA',
+  },
+};
 function Dummy() {
   return (
-    <View>
-      <Text>Dummy</Text>;
+    <View style={{ flex: 1 }}>
+      <Text>This is dummy data</Text>;
     </View>
   );
 }
 const Drawer = createDrawerNavigator<DrawerParamList>();
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Drawer.Navigator
         initialRouteName='Requisitions'
         drawerContentOptions={{
